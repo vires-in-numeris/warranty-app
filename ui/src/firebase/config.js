d87084a884a199a9582c22c9d5f08020928fb171
theanmolsharma <65589791+theanmolsharma@users.noreply.github.com>
2022-07-16 12:06:30 +0530

    firebase: init and setup

---
+++ ui/src/firebase/config.js
@@ -0,0 +1,23 @@
+import firebase from "firebase/compat";
+import 'firebase/firestore';
+import 'firebase/auth';
+import 'firebase/storage';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAudDY_RmZQk_fousdmytQDOYmpGs3dma4",
+    authDomain: "warranty-app-2a968.firebaseapp.com",
+    projectId: "warranty-app-2a968",
+    storageBucket: "warranty-app-2a968.appspot.com",
+    messagingSenderId: "1004328001986",
+    appId: "1:1004328001986:web:80577455e6dd208a73765b"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+const projectStorage = firebase.storage();
+
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { projectFirestore, projectAuth, projectStorage, timestamp };
